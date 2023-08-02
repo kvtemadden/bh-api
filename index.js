c51231be5c8facc10fc7b4889dd2f60b599eaa75
kvtemadden <katy.madden99@gmail.com>
2023-08-02 09:47:10 +0100

    change moment

--- index.js
+++ index.js
@@ -420,7 +420,7 @@ app.post("/api/broadbean", async (req, res) => {
   jobDescription = jobData.job_description;
   jobTypeId = jobData.job_type_id;
   aplitrakEmail = jobData.aplitrak_email;
-  today = moment().defaultFormat;
+  today = moment().format();
 
   console.log(today);
 
